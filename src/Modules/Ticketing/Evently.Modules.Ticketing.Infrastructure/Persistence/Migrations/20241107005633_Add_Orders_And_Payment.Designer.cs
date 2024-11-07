ae6c35fb72ef14b424d8b4b7a1e7bb6cc91712dc
Abderrahman Cheddir <acheddir@outlook.fr>
2024-11-07 02:02:35 +0100

    feat: added a new migration for additional model classes in the ticketing model

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/Persistence/Migrations/20241107005633_Add_Orders_And_Payment.Designer.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/Persistence/Migrations/20241107005633_Add_Orders_And_Payment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Evently.Modules.Ticketing.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Evently.Modules.Ticketing.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(TicketingDbContext))]
-    partial class TicketingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241107005633_Add_Orders_And_Payment")]
+    partial class Add_Orders_And_Payment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
